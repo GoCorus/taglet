3d3d330d618544469bf0dc7accd651a9bf7cc7ef
Wojnar <mateusz@sntx.pl>
2020-09-08 00:17:36 +0200

    Date fix2

--- lib/taglet.ex
+++ lib/taglet.ex
@@ -65,7 +65,7 @@ defmodule Taglet do
       taggable_type: struct.__struct__ |> taggable_type,
       context: context,
       tag_id: tag_resource.id,
-      inserted_at: DateTime.utc_now
+      inserted_at: NaiveDateTime.utc_now()
     }
   end
 
